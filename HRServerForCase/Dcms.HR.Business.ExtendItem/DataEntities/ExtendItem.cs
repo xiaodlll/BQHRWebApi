2c7828ee1ed558988d64aaf22ea1c75767aae867
xiaodl <188612307@qq.com>
2024-06-24 23:44:06 +0800

    add hrcase for api

---
+++ HRServerForCase/Dcms.HR.Business.ExtendItem/DataEntities/ExtendItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+using Dcms.Common.Torridity;
+using Dcms.Common.Torridity.Metadata;
+using System.Globalization;
+
+namespace Dcms.HR.DataEntities
+{
+
+    /// <summary>
+    /// 个案附加实体
+    /// </summary>
+    [DataEntity(PrimaryKey = "ExtendItemId")]
+    [Serializable()]
+    [Description("个案附加")]
+    public class ExtendItem : DataEntity
+    {
+
+        public const string TYPE_KEY = "ExtendItem";
+
+        #region Simple Property
+        private string _extendItemId;
+        /// <summary>
+        /// 返回/设置 主键
+        /// </summary>
+        [SimpleProperty(DbType = GeneralDbType.Guid)]
+        public string ExtendItemId
+        {
+            get { return _extendItemId; }
+            set
+            {
+                if (_extendItemId != value)
+                {
+                    _extendItemId = value;
+                    OnPropertyChanged("ExtendItemId");
+                }
+            }
+        }
+        #endregion
+    }
+}
